d6cc6a378758ae21797cdacd86f4b36cce11ce13
Dylan Martin <69947551+nalydmartin@users.noreply.github.com>
2021-01-17 15:42:19 -0800

    Updated files.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,23 +2,29 @@ import React from 'react';
 import {Container } from 'react-bootstrap';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
 
 function App() {
   return (
-    <>
+    <Router>
       <Header />
 
       <main className='py-3'>
 
         <Container>
-          <h1>Welcome to ProShop!</h1>
+          <Route path='/' component={HomeScreen} exact />
+          <Route path='/product/:id' component={ProductScreen} />
+
         </Container>
         
       </main>
 
       <Footer />
 
-    </>
+    </Router>
   );
 }
 
